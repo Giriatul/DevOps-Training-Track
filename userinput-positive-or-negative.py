0b96f3e6e933dced35cd2220bdbe47dbba433469
Atul_Giri <50609545+Giriatul@users.noreply.github.com>
2022-04-05 17:01:33 +0530

    Create userinput-positive-or-negative.py
    Check user input is a positive number or negative

---
+++ userinput-positive-or-negative.py
@@ -0,0 +1,12 @@
+user_number = input("Enter your number ")
+
+print("\n")
+try:
+    val = int(user_number)
+    if val > 0:
+        print("User number is positive ")
+    else:
+        print("User number is negative ")
+except ValueError:
+    print("No.. input string is not a number. It's a string")
+    

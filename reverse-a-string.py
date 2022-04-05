a3438e7d0063fd1be08493fca9373c97582da938
Atul_Giri <50609545+Giriatul@users.noreply.github.com>
2022-04-05 16:33:56 +0530

    Create reverse-a-string.py
    Using reverse function with join
    Python provides the reversed() function to reverse the string.

---
+++ reverse-a-string.py
@@ -0,0 +1,12 @@
+# Using reverse function with join
+# reverse a string using reversed()
+# Function to reverse a string
+def reverse(str):
+    string = "".join(reversed(str))  # reversed() function inside the join() function
+    return string
+
+
+s = "DevOps-Training_Track"
+
+print("The original string is : ", s)
+print("The reversed string using reversed() is : ", reverse(s))

69efbb3d85c3074db15409b861a843840d71f3ea
Atul_Giri <50609545+Giriatul@users.noreply.github.com>
2022-04-06 14:50:10 +0530

    Update create_dictionary.py

--- create_dictionary.py
+++ create_dictionary.py
@@ -8,7 +8,7 @@ dict1 = dict({1: 'Hello', 2: 'Hi', 3: 'Hey'})
 print("\nCreate Dictionary by using the dict() method : ")  
 print(dict1)  
   
-# Creating a Dictionary  
+# Creating a Dictionary 
 # with each item as a Pair  
 dict2 = dict([('Devansh', 90014), ('Arun', 90015)])  
 print("\nDictionary with each item as a pair: ")  
